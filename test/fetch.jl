3bf48c572115b2497c2373daaf27c05bd512514c
Jonathan Chen <jwhc@ucla.edu>
2022-02-05 22:00:54 -0800

    download test uses wordle number when digested

--- test/fetch.jl
+++ test/fetch.jl
@@ -1,9 +1,10 @@
 
 @testset "Download Wordles" begin
+    digest_wordle_number = data["wordle"]
     wordles, valid_words = Wordle.download_word_lists()
 
     @test begin
-        wordle_digest = join(wordles, ',') |> sha256 |> bytes2hex
+        wordle_digest = join(wordles[1:digest_wordle_number], ',') |> sha256 |> bytes2hex
         wordle_digest == data["data"]["known-wordles"]
     end
 
