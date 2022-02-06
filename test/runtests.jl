c4553d505bf0983f84d419badc93a6b18fae9325
Jonathan Chen <jwhc@ucla.edu>
2022-02-05 21:13:40 -0800

    add tests for data fetching (+)

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,15 +7,7 @@ using Suppressor
 
 data = TOML.parsefile(joinpath(@__DIR__(), "Data.toml"))
 
-tests = [
-    "fetch",
-    "game"
-]
-
 @testset "Wordle" begin
-    for t in tests
-        @info "Testing $t"
-        tp = joinpath(dirname(@__FILE__), "$(t).jl")
-        include(tp)
-    end
+    include("fetch.jl")
+    include("game.jl")
 end
